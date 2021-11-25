6ac626cd9af20cc7cb7150b493d58cf1ccdfed96
Chris Keele <christopher.l.keele@gmail.com>
2021-11-24 19:14:39 -0500

    some navbar work

--- content/contact.md
+++
@@ -1,9 +0,0 @@
----
-title: "Contact"
-date: 2021-11-08T21:48:47-05:00
-layout: "contact"
-draft: false
-
-contact:
-  title : "contact123"
----
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'body': '#F2F2F2',
+        light: {
+          body: '#F2F2F2',
+          text: '#F1FEF5',
+          hover: '#E5E5E5'
+        },
+        blue: {
+          DEFAULT: '#557282',
+          light: '#668393',
+          lighter: '#7798AB'
+        },
+        green: {
+          DEFAULT: '#558273'
+        }
         // 'selected-text': '#A3A3FF',
         // 'theme': '#3F3FFF',
         // 'nav': '#404053',
@@ -17,8 +29,21 @@ module.exports = {
         // 'input': '#2A2A35'
       },
       fontFamily: {
-        'roboto': ["'Roboto'", 'sans-serif']
+        roboto: ["'Roboto'", 'sans-serif']
       }
+    },
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
     },
   },
   variants: {
